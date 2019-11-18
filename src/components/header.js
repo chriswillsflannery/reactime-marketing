1cf3456a4e3897664487a71e747579be5cbad502
RajeebTheGreat <rajeebthegreat@gmail.com>
2019-11-18 14:51:57 -0500

    Deploy script and FA fix

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,11 @@ import PropTypes from "prop-types"
 import React from "react"
 import styled from 'styled-components'
 import GitHubButton from 'react-github-btn'
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChrome, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
+config.autoAddCss = false;
 
 const styles = {
   reactGreen: `#072D2B`,
