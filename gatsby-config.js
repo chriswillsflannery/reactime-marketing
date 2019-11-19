ba06b68aaf37a87d5f2421912894228338b1ece8
prasanna malla <malla.prasanna@gmail.com>
2019-11-18 19:05:10 -0500

    add ssr

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,10 @@ module.exports = {
         path: 'zopfli',
       }
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {},
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react"
 // import { Link } from "gatsby";
 // import styled from "styled-components"
